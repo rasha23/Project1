2773b41314205c057e36c59c1d7554dfe92fce01
rashajawad <rasha.jawad2015@gmail.com>
2023-05-28 11:39:27 -0400

    OddOrEven created

---
+++ src/groupStudy/OddOrEven.java
@@ -0,0 +1,11 @@
+package groupStudy;
+
+public class OddOrEven {
+    public static String identify(int number) {
+        if (number % 2 == 0) {
+            return "Even";
+        } else {
+            return "Odd";
+        }
+    }
+}
